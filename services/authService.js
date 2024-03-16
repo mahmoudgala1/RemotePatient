4c01cd7d224c4fabbb65e8d44f6a375a5b889662
Mahmoud Galal <mahmoudgalal173.95@gmail.com>
2024-03-16 15:56:31 +0200

    edit

--- services/authService.js
+++ services/authService.js
@@ -1,31 +1,31 @@
 const asyncHandelr = require("express-async-handler");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const dbConnection = require("../config/database");
 const ApiError = require("../utils/apiError");
 const sendMail = require("../utils/sendEmail");
 const { validateCreateUser, validateLogineUser } = require("../models/userModel");
 
-async function hashPassword(pass) {
-    const saltRounds = 10;
-    const hashedPassword = await new Promise((resolve, reject) => {
-        bcrypt.hash(pass, saltRounds, function (err, hash) {
-            if (err) reject(err)
-            resolve(hash)
-        });
-    })
-    return hashedPassword
-}
+// async function hashPassword(pass) {
+//     const saltRounds = 10;
+//     const hashedPassword = await new Promise((resolve, reject) => {
+//         bcrypt.hash(pass, saltRounds, function (err, hash) {
+//             if (err) reject(err)
+//             resolve(hash)
+//         });
+//     })
+//     return hashedPassword
+// }
 
-async function comparePassword(plainPass, hashword) {
-    const result = await new Promise((resolve, reject) => {
-        bcrypt.compare(plainPass, hashword, function (err, isPasswordMatch) {
-            if (err) reject(err)
-            resolve(isPasswordMatch)
-        });
-    })
-    return result
-}
+// async function comparePassword(plainPass, hashword) {
+//     const result = await new Promise((resolve, reject) => {
+//         bcrypt.compare(plainPass, hashword, function (err, isPasswordMatch) {
+//             if (err) reject(err)
+//             resolve(isPasswordMatch)
+//         });
+//     })
+//     return result
+// }
 
 const register = asyncHandelr(async (req, res, next) => {
     const { error } = validateCreateUser(req.body);
@@ -39,9 +39,9 @@ const register = asyncHandelr(async (req, res, next) => {
     }
     const {
         fullName, email, password } = req.body;
-    const hashedPassword = await hashPassword(password);
+    // const hashedPassword = await hashPassword(password);
     const [result2] = await (await dbConnection).query(`INSERT INTO users (fullName,email,password)
-        VALUES(?,?,?)`, [fullName, email, hashedPassword]);
+        VALUES(?,?,?)`, [fullName, email, password]);
     const [result3] = await (await dbConnection).query('SELECT id FROM users WHERE email = ?', [req.body.email]);
     const verificationCode = Math.floor(Math.random() * 9000) + 1000;
     sendMail(email, "verification Code", verificationCode);
@@ -58,8 +58,9 @@ const login = asyncHandelr(async (req, res, next) => {
         res.status(404).json({ message: "Wrong Email Or Password" });
         return next(new ApiError(`Wrong Email Or Password`, 404));
     }
-    const passwordMatch = comparePassword(req.body.password, result[0].password);
-    if (!passwordMatch) {
+    // const passwordMatch = comparePassword(req.body.password, result[0].password);
+    if (!(req.body.password == result[0].password)) {
+        res.status(404).json({ message: "Wrong Email Or Password" });
         return next(new ApiError(`Wrong Email Or Password`, 404));
     }
     res.status(200).json({ Id: result[0].id });
