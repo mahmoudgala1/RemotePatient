0907b11efd69f5f3e3860ce4a4eb53f523ef284b
Mahmoud Galal <mahmoudgalal173.95@gmail.com>
2024-03-16 08:01:00 +0200

    ))

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 const dbConnection = mysql.createConnection({
     host: process.env.MYSQL_URL,
-    user: MYSQL_USER,
-    password: MYSQL_PASS,
-    database: MYSQL_DATABASE_NAME,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DATABASE_NAME,
     multipleStatements: true
 });
 module.exports = dbConnection;
